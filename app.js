df70189baae76ee1b70cb41d1346635cd94ea25d
Gumer Laura TM <gumeranthonylauraquentasis@gmail.com>
2024-07-09 19:49:49 -0500

    Modificando errores en el archivo app.js

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const replaceText = [
     ["u", "ufat"]];
 
 // Elementos DOM
-const textArea = document.querySelector(".text-area");
+const textArea = document.querySelector(".text_area");
 const mensaje = document.querySelector(".message");
 const copia = document.querySelector(".copy");
+const btncopia = document.querySelector(".btn-copy");
+const mensaje1 = document.querySelector(".salida_titulo");
+const mensaje2 = document.querySelector(".salida_parrafo");
+
+
 
 //Funciones operacionales
 function validateText(){
@@ -22,9 +27,24 @@ function validateText(){
     }
 }
 
+function validateText(){
+    let res =  new RegExp("^[a-zA-Z]+$").test(textArea.value);
+    let res2 =  new RegExp("^[a-zA-Z]+ +$").test(textArea.value);
+    console.log( res,res2);
+    if (!res && !res2) {
+        alert("Argumentos no validos");
+        return true;
+    } else {
+        alert("Palabra Aceptada.");
+    }
+}
+
 function replace_encr(text,tipe){
     for(let i = 0; i < replaceText.length; i++)
         if(text.includes(replaceText[i][0])) text = text.replaceAll(replaceText[i][tipe?0:1], replaceText[i][tipe?1:0]);
+    btncopia.style.display = "block";
+    mensaje1.style.display = "none";
+    mensaje2.style.display = "none";
     return text;
 }
 
@@ -33,7 +53,7 @@ function clear_text_area(){
 }
 
 //Funciones de los botones
-function btnEncript(){
+function btn_encript(){
     if(!validateText()) {
         mensaje.value = replace_encr(textArea.value.toLowerCase(),true);
         mensaje.style.backgroundImage = "none"
@@ -41,7 +61,7 @@ function btnEncript(){
     }
 }
 
-function btnDecript(){
+function btn_decript(){
     if(!validateText()) {
         mensaje.value = replace_encr(textArea.value.toLowerCase(),false);
         mensaje.style.backgroundImage = "none"
@@ -49,7 +69,7 @@ function btnDecript(){
     }
 }
 
-function btnCopy(){
+function btn_copy(){
     if (mensaje.value) {
         navigator.clipboard.writeText(mensaje.value)
         alert("Texto Copiado")
